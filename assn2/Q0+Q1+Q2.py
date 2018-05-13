d60bf9922427c32f35d83cd159226cc14eb0db2a
brettmccausland <brettmccausland@yahoo.com>
2018-05-13 14:45:59 -0700

    cleaning

--- assn2/Q0+Q1+Q2.py
+++ assn2/Q0+Q1+Q2.py
@@ -140,48 +140,6 @@ def Q1(data):
 # 10 being 1 Accurcy, 1 Sensitivity, 1 Specificity for each data split
 def tenFoldEvaluation(data,Accurcy,Sensitivity,Specificity,k,p):
 
- data = data.sample(frac=1).reset_index(drop=True)
- #split data into 10 folds
- #dataArr is a array of 10 arrays of data tuples
- dataArr = np.array_split(data, 10)
-
- #stores training tuples
- trainingtuples = []
-
- for i in range(10):  # i selects the test
-   for j in range(10):# j selects the training data
-     if(j!=i):        # if not i it becomes part of the traning data
-       trainingtuples.append(dataArr[j])
-
-   # Splitting the data into the folds
-
-   # create a table with the training data
-   x_train=pd.concat(trainingtuples)  #let x_train = traingtuples collection
-
-   y_train = x_train.iloc[:,9].values #save class labels
-   x_train=x_train.iloc[:,:-1].values #get rid of labels
-
-   #Gathering the testing data
-   x_test= dataArr[i] # the test data is i
-   y_test = x_test.iloc[:,9].values # save the class lables y
-   x_test=x_test.iloc[:,:-1].values # get rid of the class labels x
-
-   #ypred[] will return labels array that should match the testing labels
-
-   ypred = knn_classifier(x_train,x_test, y_train, k, p)
-
-   #call and append the evaluation %'s
-   Accurcy.append([accuracy(ypred,y_test),k,p])
-   Sensitivity.append([sensitivity(ypred,y_test),k,p])
-   Specificity.append([specificity(ypred,y_test),k,p])
-   trainingtuples.clear()
- return
-# postcondition of 1 call:
-# Runs Knn 10 times
-# finds and the evaluation of each call
-# Saves all 10 Accurcy,10 Sensitivity, 10 Specificity readings
-# 10 being 1 Accurcy, 1 Sensitivity, 1 Specificity for each data split
-def tenFoldEvaluationq2(data,Accurcy,Sensitivity,Specificity,k,p):
 
  data = data.sample(frac=1).reset_index(drop=True)
  #split data into 10 folds
@@ -219,67 +177,127 @@ def tenFoldEvaluationq2(data,Accurcy,Sensitivity,Specificity,k,p):
    Specificity.append([specificity(ypred,y_test)])
    trainingtuples.clear()
  return
+# postcondition of 1 call:
+# Runs Knn 10 times
+# finds and the evaluation of each call
+# Saves all 10 Accurcy,10 Sensitivity, 10 Specificity readings
+# 10 being 1 Accurcy, 1 Sensitivity, 1 Specificity for each data split
+# within each fold of the cross-validation, record mean and standard
+# deviation of the performance for k = 1:10, for p = 1 and p =2
+def Q2_2(data):
+#power 1
+ Maccurcy_p1 = []
+ Mspecificity_p1=[]
+ Msensitivity_p1=[]
+ #standard deviation
+ STDMsensitivity_p1=[]
+ STDaccurcy_p1=[]
+ STDMspecificity_p1=[]
 
-#precondition: Q1
-#Accurcy: percentage for
-#postcondition: plots = each power,for the evaluations gathered for all 10 folds
-def Q2Plots(Accurcy,Sensitivity,Specificity):
- # ------ Accurcy Decomposition -------
+ # power 2
+ Maccurcy_p2 = []
+ Mspecificity_p2 =[]
+ Msensitivity_p2 =[]
+ #standard deviation
+ STDMsensitivity_p2 =[]
+ STDaccurcy_p2 =[]
+ STDMspecificity_p2 =[]
 
- Accurcy.sort(key=operator.itemgetter(2))
- AccurcyTable=pd.DataFrame(list(Accurcy))
+ #shuffle data
 
-             # ------ p=1 -------
- Acc_p1=AccurcyTable.iloc[0:90,0].values
- Acc_k_p1=AccurcyTable.iloc[0:90,1].values
- plt.scatter(Acc_k_p1,Acc_p1)
- plt.title('Accurcy power 1')
- plt.figure()
-             # ------ p=2 -------
- Acc_p2=AccurcyTable.iloc[90:180,0].values
- Acc_k_p2=AccurcyTable.iloc[90:180,1].values
- plt.scatter(Acc_k_p2,Acc_p2)
- plt.title('Accurcy power 2')
- plt.figure()
+ # 30 times
+ for k in range(1,11): #for 10 k nieghbors
+  for p in range(1,3):  #for 2 powers
+    tenFoldEvaluation(data,Accurcy,Sensitivity,Specificity,k,p)
+    if(p==1):
+    #MEANS
+     Maccurcy_p1.append(np.mean(Accurcy))#([accuracy(ypred,y_test),k,p])
+     Msensitivity_p1.append(np.mean(Sensitivity))
+     Mspecificity_p1.append(np.mean(Specificity))
+     #STANDARD DEVIATIONS
+     STDaccurcy_p1.append(np.std(Accurcy))
+     STDMsensitivity_p1.append(np.std(Sensitivity))
+     STDMspecificity_p1.append(np.std(Specificity))
+    else:
+     #MEANS
+     Maccurcy_p2.append(np.mean(Accurcy))#([accuracy(ypred,y_test),k,p])
+     Msensitivity_p2.append(np.mean(Sensitivity))
+     Mspecificity_p2.append(np.mean(Specificity))
+     #STANDARD DEVIATIONS
+     STDaccurcy_p2.append(np.std(Accurcy))
+     STDMsensitivity_p2.append(np.std(Sensitivity))
+     STDMspecificity_p2.append(np.std(Specificity))   
+   
+    Accurcy.clear()
+    Sensitivity.clear()
+    Specificity.clear()
 
- # ------ Sensitivity Decomposition -------
+ k_list=[1,2,3,4,5,6,7,8,9,10]
+ Xlabel='nieghbors'
+ Ylabel='performance'
+ #---------------------------Accurcy-----------------------------
+ 
+ #       ----------------- Power 1 -----------
+ 
+ plt.errorbar(x= k_list, y=Maccurcy_p1,yerr=STDaccurcy_p1)
+ plt.title('Accurcy Power 1')
+ plt.xlabel(Xlabel) 
+ plt.ylabel(Ylabel)
+ plt.savefig('AccurcyPower1')
+ plt.close()
+ #               --------- Power 2 -----------
 
- Sensitivity.sort(key=operator.itemgetter(2))
- SensitivityTable=pd.DataFrame(list(Sensitivity))
+ plt.errorbar(x= k_list, y=Maccurcy_p2,yerr=STDaccurcy_p2)
+ plt.title('Accurcy Power 2')
+ plt.xlabel(Xlabel) 
+ plt.ylabel(Ylabel)
+ plt.savefig('AccurcyPower2')
+ plt.close()
 
-             # ------ p=1 -------
- Sens_p1=SensitivityTable.iloc[0:90,0].values
- Sens_k_p1=SensitivityTable.iloc[0:90,1].values
- plt.scatter(Sens_k_p1,Sens_p1, c="g", alpha=0.4,marker='^')
- plt.ylabel('Sensitivity')
- plt.xlabel('k')
- plt.title('Sensitivity')
- plt.plot(Sens_k_p1,Sens_p1, linewidth=2.0)
- plt.figure()
-              # ------ p=2 -------
- Sens_p2=SensitivityTable.iloc[90:180,0].values
- Sens_k_p2=SensitivityTable.iloc[90:180,1].values
- plt.scatter(Sens_k_p2,Sens_p2, c="b", alpha=0.4,marker='o')
- plt.legend()
- plt.figure()
- #plt.title('Sensitivity power 2')
+ #---------------------------Sensitivity-----------------------------
+ 
+ #       ----------------- Power 1 -----------
+ 
+ plt.errorbar(x= k_list, y=Msensitivity_p1,yerr=STDMsensitivity_p1)
+ plt.title('Sensitivity Power 1')
+ plt.xlabel(Xlabel) 
+ plt.ylabel(Ylabel)
+ plt.savefig('SensitivityPower1')
+ plt.close()
+ #               --------- Power 2 -----------
 
- # ------ Specificity Decomposition -------
- Specificity.sort(key=operator.itemgetter(2))
- SpecificityTable=pd.DataFrame(list(Specificity))
-              # ------ p=1 -------
- Spec_p1=SpecificityTable.iloc[0:90,0].values
- Spec_k_p1=SpecificityTable.iloc[0:90,1].values
- plt.scatter(Spec_p1,Spec_k_p1)
- plt.title('Specificity power 1')
- plt.figure();
-              # ------ p=2 -------
- Spec_p2=SpecificityTable.iloc[90:180,0].values
- Spec_k_p2=SpecificityTable.iloc[90:180,1].values
- plt.scatter(Spec_p2,Spec_k_p2)
- plt.title('Specificity power 2')
- plt.figure()
+ plt.errorbar(x= k_list,y=Msensitivity_p2,yerr=STDMsensitivity_p2)
+ plt.title('Sensitivity Power 2')
+ plt.xlabel(Xlabel) 
+ plt.ylabel(Ylabel)
+ plt.savefig('SensitivityPower2')
+ plt.close()
+
+ #---------------------------Specificity-----------------------------
+ 
+ #                   -------- Power 1 -------
+ 
+ plt.errorbar(x= k_list, y=Mspecificity_p1,yerr=STDMspecificity_p1)
+ plt.title('Specificity Power 1')
+ plt.xlabel(Xlabel) 
+ plt.ylabel(Ylabel)
+ plt.savefig('SpecificityPower1')
+ plt.close()
+ #                   -------- Power 2 --------
+
+ plt.errorbar(x= k_list,y=Mspecificity_p2,yerr=STDMspecificity_p2)
+ plt.title('Specificity Power 2')
+ plt.xlabel(Xlabel) 
+ plt.ylabel(Ylabel)
+ plt.savefig('SpecificityPower2')
+ plt.close()
+ 
+#----------------------*--------------------------------
  return
+
+
+
+
 
 
 # -------------------MAIN()--------------------------
@@ -289,119 +307,7 @@ Specificity=[]
 
 data = Q0()
 Q1(data)
-#------------------Question 2-------------------------
-# within each fold of the cross-validation, record mean and standard
-# deviation of the performance for k = 1:10, for p = 1 and p =2
-
-#power 1
-Maccurcy_p1 = []
-Mspecificity_p1=[]
-Msensitivity_p1=[]
-#standard deviation
-STDMsensitivity_p1=[]
-STDaccurcy_p1=[]
-STDMspecificity_p1=[]
-
-# power 2
-Maccurcy_p2 = []
-Mspecificity_p2 =[]
-Msensitivity_p2 =[]
-#standard deviation
-STDMsensitivity_p2 =[]
-STDaccurcy_p2 =[]
-STDMspecificity_p2 =[]
-
-#shuffle data
-
-# 30 times
-for k in range(1,11): #for 10 k nieghbors
- for p in range(1,3):  #for 2 powers
-   tenFoldEvaluationq2(data,Accurcy,Sensitivity,Specificity,k,p)
-   if(p==1):
-    #MEANS
-    Maccurcy_p1.append(np.mean(Accurcy))#([accuracy(ypred,y_test),k,p])
-    Msensitivity_p1.append(np.mean(Sensitivity))
-    Mspecificity_p1.append(np.mean(Specificity))
-    #STANDARD DEVIATIONS
-    STDaccurcy_p1.append(np.std(Accurcy))
-    STDMsensitivity_p1.append(np.std(Sensitivity))
-    STDMspecificity_p1.append(np.std(Specificity))
-   else:
-    #MEANS
-    Maccurcy_p2.append(np.mean(Accurcy))#([accuracy(ypred,y_test),k,p])
-    Msensitivity_p2.append(np.mean(Sensitivity))
-    Mspecificity_p2.append(np.mean(Specificity))
-    #STANDARD DEVIATIONS
-    STDaccurcy_p2.append(np.std(Accurcy))
-    STDMsensitivity_p2.append(np.std(Sensitivity))
-    STDMspecificity_p2.append(np.std(Specificity))   
-   
-   Accurcy.clear()
-   Sensitivity.clear()
-   Specificity.clear()
-
-k_list=[1,2,3,4,5,6,7,8,9,10]
-Xlabel='nieghbors'
-Ylabel='performance'
- #---------------------------Accurcy-----------------------------
- 
- #       ----------------- Power 1 -----------
- 
-plt.errorbar(x= k_list, y=Maccurcy_p1,yerr=STDaccurcy_p1)
-plt.title('Accurcy Power 1')
-plt.xlabel(Xlabel) 
-plt.ylabel(Ylabel)
-plt.savefig('AccurcyPower1')
-plt.close()
- #               --------- Power 2 -----------
-
-plt.errorbar(x= k_list, y=Maccurcy_p2,yerr=STDaccurcy_p2)
-plt.title('Accurcy Power 2')
-plt.xlabel(Xlabel) 
-plt.ylabel(Ylabel)
-plt.savefig('AccurcyPower2')
-plt.close()
-
- #---------------------------Sensitivity-----------------------------
- 
- #       ----------------- Power 1 -----------
- 
-plt.errorbar(x= k_list, y=Msensitivity_p1,yerr=STDMsensitivity_p1)
-plt.title('Sensitivity Power 1')
-plt.xlabel(Xlabel) 
-plt.ylabel(Ylabel)
-plt.savefig('SensitivityPower1')
-plt.close()
- #               --------- Power 2 -----------
-
-plt.errorbar(x= k_list,y=Msensitivity_p2,yerr=STDMsensitivity_p2)
-plt.title('Sensitivity Power 2')
-plt.xlabel(Xlabel) 
-plt.ylabel(Ylabel)
-plt.savefig('SensitivityPower2')
-plt.close()
-
- #---------------------------Specificity-----------------------------
- 
- #                   -------- Power 1 -------
- 
-plt.errorbar(x= k_list, y=Mspecificity_p1,yerr=STDMspecificity_p1)
-plt.title('Specificity Power 1')
-plt.xlabel(Xlabel) 
-plt.ylabel(Ylabel)
-plt.savefig('SpecificityPower1')
-plt.close()
- #                   -------- Power 2 --------
-
-plt.errorbar(x= k_list,y=Mspecificity_p2,yerr=STDMspecificity_p2)
-plt.title('Specificity Power 2')
-plt.xlabel(Xlabel) 
-plt.ylabel(Ylabel)
-plt.savefig('SpecificityPower2')
-plt.close()
- 
-#----------------------*--------------------------------
-
+Q2_2(data)
 
 
 
